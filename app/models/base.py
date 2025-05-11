004340d8525907f7a2cd33221a1f3471ba75165f
An4nasik <144912844+An4nasik@users.noreply.github.com>
2025-05-11 13:13:19 +0300

    Update base.py

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel as PydanticBaseModel
 from bson import ObjectId
 from datetime import date
+
+
 class BaseModel(PydanticBaseModel):
     model_config = {
         "json_schema_extra": {},
